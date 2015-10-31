4fb73d5c66445a411a87d1fa1b6c07f8e3b2b52a
Alchan Kim <a9413miky@gmail.com>
2015-10-31 21:47:06 +0900

    ADded option

--- croppers/test3.cpp
+++ croppers/test3.cpp
@@ -1,4 +1,7 @@
 #include <cstdlib>
+#include <getopt.h>
+
+#include <string>
 #include <map>
 #include <iostream>
 #include <algorithm>
@@ -10,9 +13,191 @@
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/ml/ml.hpp>
-#define FLOODFILL_MAGIC 1
 
+#define FLOODFILL_MAGIC 1
 using namespace std;
+
+class Config {
+private:
+    vector<string> optNames;
+    map<string, string*> optNameStrRefMap; // long name -> string *
+    map<string, int*> optNameIntRefMap; // long name -> int *
+    map<string, bool*> optNameBoolRefMap; // long name -> bool *
+    map<string, string> optNameDescMap; // long name -> description
+    vector<struct option> optStructs;
+    bool optPadded = false;
+private:
+    bool _addOpt(string& name, int has_arg, string& desc) {
+        if (optPadded) {
+            return false;
+        }
+        optNames.push_back(name);
+        optStructs.push_back((struct option){optNames.back().c_str(), has_arg, 0, 0});
+        optNameDescMap[name] = desc;
+        return true;
+    }
+
+    bool addOpt(string name, int has_arg, string desc, string* ref) {
+        if (!_addOpt(name, has_arg, desc))
+            return false;
+        optNameStrRefMap[name] = ref;
+        return true;
+    }
+
+    bool addOpt(string name, int has_arg, string desc, int* ref) {
+        if (!_addOpt(name, has_arg, desc))
+            return false;
+        optNameIntRefMap[name] = ref;
+        return true;
+    }
+
+    bool addOpt(string name, int has_arg, string desc, bool* ref) {
+        if (!_addOpt(name, has_arg, desc))
+            return false;
+        optNameBoolRefMap[name] = ref;
+        return true;
+    }
+
+    bool postParse();
+    bool parseOpt(int argc, char** argv) {
+        if (!optPadded) {
+            optPadded = true;
+            optStructs.push_back((struct option){0, 0, 0, 0});
+        }
+        struct option *options = optStructs.data();
+        int ind;
+        int c;
+        while ((c = getopt_long_only(argc, argv, "", options, &ind)) != -1) {
+            if (c == '?') {
+                printOpt(argv[0]);
+                return false;
+            }
+            string name = options[ind].name;
+
+            if (optNameIntRefMap.find(name) != optNameIntRefMap.end()) {
+                int* ref = optNameIntRefMap[name];
+                *ref = atoi(optarg);
+            } else if (optNameStrRefMap.find(name) != optNameStrRefMap.end()) {
+                string* ref = optNameStrRefMap[name];
+                *ref = optarg;
+            } else if (optNameBoolRefMap.find(name) != optNameBoolRefMap.end()) {
+                bool* ref = optNameBoolRefMap[name];
+                string optArg = optarg;
+                if (optArg == "y" || optArg == "yes" || optArg == "t" || optArg == "true" || optArg == "1") {
+                    *ref = true;
+                } else {
+                    *ref = false;
+                }
+            }
+            if (!postParse())
+                return false;
+        }
+        return true;
+    }
+
+    void printOpt(const char* arg0) {
+        cerr << arg0 << " ";
+        for (int idx = 0; idx < optStructs.size(); idx++) {
+            struct option opt = optStructs[idx];
+            if (!opt.name)
+                break;
+            cerr << "[--" << opt.name << "] ";
+        }
+        cerr << endl;
+        cerr << "--------" << endl << "Usage" << endl << endl;
+
+        for (int idx = 0; idx < optStructs.size(); idx++) {
+            struct option *opt = &optStructs[idx];
+            if (!opt->name)
+                break;
+            cerr << "--" << opt->name << ": ";
+            cerr << optNameDescMap[string(opt->name)] << endl;
+        }
+    }
+
+public:
+    string imageFilename;
+    int cannyLevel = 40;
+    int blurSize = 2;
+    int kernelSize = 1;
+    int gridCount = 50;
+    
+    string _pinpointStr;
+    cv::Scalar pinpointBegin = cv::Scalar(20, 20, 100);
+    cv::Scalar pinpointEnd = cv::Scalar(60, 150, 255);
+
+    bool verbose = false;
+
+    string dbgFilename;
+    string dbgPinpointFilename;
+    string dbgCannyFilename;
+public:
+    Config() {
+        addOpt("input", required_argument, "File name. If not specified, program reads an image from stdin", &this->imageFilename);
+        addOpt("canny-level", required_argument, "Canny level", &this->cannyLevel);
+        addOpt("blur-size", required_argument, "Size of kernel used in the gaussian blur phase", &this->blurSize);
+        addOpt("kernel-size", required_argument, "Size of kernel used in the dilate phase", &this->kernelSize);
+        addOpt("grid-count", required_argument, "Count of grid edges in an axis", &this->gridCount);
+        addOpt("pinpoint-range", required_argument, "Pinpoint range in HSV color space (e.g 20,20,100:60,150,255)", &this->_pinpointStr);
+        addOpt("verbose", required_argument, "Verbose mode", &this->verbose);
+        addOpt("dbg-image", required_argument, "Path to which image footprint will be saved", &this->dbgFilename);
+        addOpt("dbg-pinpoint-image", required_argument, "", &this->dbgPinpointFilename);
+        addOpt("dbg-canny-image", required_argument, "", &this->dbgCannyFilename);
+    }
+
+    void loadFromArgv(int argc, char **argv) {
+        if (!parseOpt(argc, argv)) {
+            printOpt(argv[0]);
+            exit(1);
+        }
+    }
+
+    void log(string msg) {
+        if (verbose)
+            cerr << msg << endl;
+    }
+
+} config;
+
+bool Config::postParse() {
+    if (_pinpointStr == "") {
+        return true;
+    }
+
+    size_t idx = _pinpointStr.find(":");
+    if (idx == _pinpointStr.npos) {
+        return false;
+    }
+
+    string before = _pinpointStr.substr(0, idx);
+    string after = _pinpointStr.substr(idx + 1);
+
+    for (int k = 0; k <= 1; k++) {
+        cv::Scalar& dest = k == 0? pinpointBegin : pinpointEnd;
+        string& line = k == 0? before : after;
+
+        int cnt = 0;
+        int values[3];
+        int lastidx = 0;
+        int idx;
+        while (cnt < 3) {
+            idx = line.find(",", lastidx);
+            string part = line.substr(lastidx, idx - lastidx);
+            lastidx = idx + 1;
+            values[cnt++] = atoi(part.c_str());
+            if (idx == line.npos) {
+                break;
+            }
+        }
+        if (cnt < 3 || idx != line.npos) {
+            return false;
+        }
+        dest = cv::Scalar(values[0], values[1], values[2]);
+    }
+    _pinpointStr = "";
+    return true;
+}
+
 class Filler {
 public:
     cv::Mat mask;
@@ -42,7 +227,6 @@ Filler::Filler(cv::Mat img, cv::Mat pinpointImg, cv::Mat canniedImg)
 
 void Filler::makeMask() {
     cv::copyMakeBorder(canniedImg, mask, 1, 1, 1, 1, cv::BORDER_CONSTANT, cv::Scalar(255));
-    cv::imwrite("/home/algy/cvd/mask_initial.jpg", mask);
 }
 
 void Filler::fill() {
@@ -54,8 +238,8 @@ void Filler::fill() {
     int height = img.size().height,
         width = img.size().width;
 
-    int cell_height = height / 50,
-        cell_width = width / 50;
+    int cell_height = max(height / config.gridCount, 1),
+        cell_width = max(width / config.gridCount, 1);
     for (x = width % cell_width / 2; x < width; x += cell_width) {
         for (y = height % cell_height / 2; y < height; y += cell_height) {
             if (pinpointImg.at<uchar>(y, x) == 0)
@@ -75,7 +259,7 @@ inline bool Filler::iterFloodFill(int x, int y, int magic, vector<cv::Point>& ve
 }
 
 inline bool Filler::loopFloodFill(int magic, cv::Rect rect, vector<cv::Point>& indices) {
-    cerr << "EXTRACTING POINTS " << magic << endl;
+    config.log("EXTRACTING POINTS");
 
     bool entered = false;
     int x, k;
@@ -135,14 +319,14 @@ void Filler::iterPinpoint(int x, int y) {
         return;
 
     if (FLOODFILL_MAGIC + inc == 255) {
-        cerr << "Warnning: Too many pinpointed areas" << endl;
+        config.log("Warnning: Too many pinpointed areas");
         makeMask();
         inc = 0;
     }
     int magic = FLOODFILL_MAGIC + inc;
     // int flags = 4 | cv::FLOODFILL_MASK_ONLY | magic << 8;
     int flags = 4;
-    cerr << "FLOOD FILLING " << inc << endl;
+    config.log("FLOOD FILLING");
     
     cv::Rect boundingBox;
     long long area = (int)cv::floodFill(mask, cv::Point(x + 1, y + 1), magic,
@@ -173,84 +357,108 @@ void Filler::iterPinpoint(int x, int y) {
 
 }
 
-int main (int argc, char** argv) {
-    if (argc < 2) {
-        cerr << "./program image" << endl;
-        return 1;
-    }
-    char *filename = argv[1];
-    int idx, kdx;
+int main(int argc, char** argv) {
+    config.loadFromArgv(argc, argv);
 
-    cv::Mat img = cv::imread(filename);
+    if (config.imageFilename == "") {
+        config.imageFilename = "/dev/stdin";
+    }
+
+    int idx, kdx;
+    cv::Mat img = cv::imread(config.imageFilename);
 
     if (img.data == NULL) {
-        cerr << "Cannot read input image" << endl;
+        cerr << "Cannot read input file \"" << config.imageFilename << "\"" << endl;
         return 1;
     }
+
     cv::Mat canniedImg;
-    cerr << "Cannying" << endl;
-    cv::Canny(img, canniedImg, 20, 40);
-    cerr << "Done" << endl;
-    cv::imwrite("/home/algy/cvd/canny_orig.jpg", canniedImg);
+    config.log("Cannying");
+    cv::Canny(img, canniedImg, config.cannyLevel / 2, config.cannyLevel);
+    config.log("Done");
         
-    cerr << "Blurring" << endl;
-    cv::GaussianBlur(canniedImg, canniedImg, cv::Size(5, 5), 0);
-    cv::threshold(canniedImg, canniedImg, 0, 255, cv::THRESH_BINARY);
-    {
-        cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3), cv::Point(1, 1));
-        cv::dilate(canniedImg, canniedImg, element);
+    if (config.blurSize > 0 || config.kernelSize > 0) {
+        config.log("Blurring");
+        if (config.blurSize > 0) {
+            cv::GaussianBlur(
+                canniedImg,
+                canniedImg,
+                cv::Size(config.blurSize * 2 + 1, config.blurSize * 2 + 1),
+                0);
+            cv::threshold(canniedImg, canniedImg, 0, 255, cv::THRESH_BINARY);
+        }
+        if (config.kernelSize > 0) {
+            cv::Mat element = cv::getStructuringElement(
+                cv::MORPH_ELLIPSE,
+                cv::Size(config.kernelSize * 2 + 1, config.kernelSize * 2 + 1),
+                cv::Point(config.kernelSize, config.kernelSize));
+            cv::dilate(canniedImg, canniedImg, element);
+        }
+        config.log("Done");
     }
-    cerr << "Done" << endl;
-    cv::imwrite("/home/algy/cvd/canny.jpg", canniedImg);
+
+    if (config.dbgCannyFilename != "") {
+        cv::imwrite(config.dbgCannyFilename.c_str(), canniedImg);
+    }
 
     cv::Mat hsvImg;
     cv::Mat pinpointImg;
-    cerr << "Pinpointing" << endl;
+    config.log("Pinpointing");
     cv::cvtColor(img, hsvImg, cv::COLOR_BGR2HSV);
     cv::inRange(hsvImg,
-        cv::Scalar(20, 20, 100),
-        cv::Scalar(60, 150, 255),
+        config.pinpointBegin,
+        config.pinpointEnd,
         pinpointImg);
     cv::subtract(pinpointImg, canniedImg, pinpointImg);
-    cerr << "Done" << endl;
-    cv::imwrite("/home/algy/cvd/pinpoint.jpg", pinpointImg);
+    if (config.dbgPinpointFilename != "") {
+        cv::imwrite(config.dbgPinpointFilename.c_str(), pinpointImg);
+    }
 
     Filler filler(img, pinpointImg, canniedImg);
     filler.fill();
 
-    map<uchar, cv::Vec3b> colorCache;
-    for (int y = 0; y < img.size().height; y++) {
-        for (int x = 0; x < img.size().width; x++) {
-            uchar val = filler.mask.at<uchar>(y + 1, x + 1);
-            if (val != 0) {
-                cv::Vec3b &b = img.at<cv::Vec3b>(y, x);
-                if (colorCache.find(val) == colorCache.end()) {
-                    cv::Vec3b color(rand() % 255, rand() % 255, rand() % 255);
-                    colorCache[val] = color;
+    bool dbgOut = config.dbgFilename != "";
+    if (dbgOut) {
+        map<uchar, cv::Vec3b> colorCache;
+        for (int y = 0; y < img.size().height; y++) {
+            for (int x = 0; x < img.size().width; x++) {
+                uchar val = filler.mask.at<uchar>(y + 1, x + 1);
+                if (val != 0) {
+                    cv::Vec3b &b = img.at<cv::Vec3b>(y, x);
+                    if (colorCache.find(val) == colorCache.end()) {
+                        cv::Vec3b color(rand() % 255, rand() % 255, rand() % 255);
+                        colorCache[val] = color;
+                    }
+                    b = colorCache[val];
                 }
-                b = colorCache[val];
             }
         }
     }
+
     size_t length = filler.resultPoints.size();
     cout << length << endl << endl;
     for (idx = 0; idx < length; idx++) {
         cv::Point pnt = filler.resultPoints[idx];
         cout << "[" << pnt.x << ", " << pnt.y << "]" << endl;
 
-        cv::circle(img, pnt, 3, cv::Scalar(0, 0, 255), 3);
+        if (dbgOut)
+            cv::circle(img, pnt, 3, cv::Scalar(0, 0, 255), 3);
         for (kdx = 0; kdx < 4; kdx++) {
             cv::Point rectPnt = filler.resultBoxes[idx * 4 + kdx];
             cout << rectPnt.x << " " << rectPnt.y << endl;
 
-            cv::line(img,
-                filler.resultBoxes[idx * 4 + kdx],
-                filler.resultBoxes[idx * 4 + (kdx + 1)%4],
-                cv::Scalar(0, 255, 0),
-                3);
+            if (dbgOut) {
+                cv::line(img,
+                    filler.resultBoxes[idx * 4 + kdx],
+                    filler.resultBoxes[idx * 4 + (kdx + 1)%4],
+                    cv::Scalar(0, 255, 0),
+                    3);
+            }
         }
-        cout << endl << endl;
+        cout << endl;
     }
-    cv::imwrite("/home/algy/cvd/a.jpg", img);
+    if (dbgOut) {
+        cv::imwrite(config.dbgFilename, img);
+    }
     return 0;
 }
